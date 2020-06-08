f376756f0330085df08ff30793daf2fff5c691a0
hongdongsu <dsh@storecamera.io>
2020-06-09 00:53:15 +0900

    title 설정자, 접근자 추가

--- 10_capsulation_embedding/main.go
+++ 10_capsulation_embedding/main.go
@@ -8,7 +8,13 @@ import (
 
 func main() {
 	event := calendar.Event{}
-	err := event.SetYear(2019)
+
+	err := event.SetTitle("Mom's birthday")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = event.SetYear(2019)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,6 +29,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	fmt.Println(event.Title())
 	fmt.Println(event.Year())
 	fmt.Println(event.Month())
 	fmt.Println(event.Day())
