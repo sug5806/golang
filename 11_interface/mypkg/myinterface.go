eb4df2bec045a8ea088972c140ff5a0e853e47fc
hongdongsu <dsh@storecamera.io>
2020-06-09 21:35:57 +0900

    interface 추가 및 gitignore 추가

---
+++ 11_interface/mypkg/myinterface.go
@@ -0,0 +1,25 @@
+package mypkg
+
+import "fmt"
+
+type MyInterface interface {
+	MethodWithoutParameters()
+	MethodWithParameter(float64)
+	MethodWithReturnValue() string
+}
+
+type MyType int
+
+func (m MyType) MethodWithoutParameters() {
+	fmt.Println("MethodWithoutParameters called")
+}
+func (m MyType) MethodWithParameter(f float64) {
+	fmt.Println("MethodWithParameter called with ", f)
+}
+func (m MyType) MethodWithReturnValue() string {
+	return "Hi MethodWithReturnValue"
+}
+
+func (m MyType) MethodNotInInterface() {
+	fmt.Println("MethodNotInInterface called")
+}
