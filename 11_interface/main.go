8e72205c036d40f7d85dc0e56f432ffbec4a48b1
hongdongsu <dsh@storecamera.io>
2020-06-09 21:49:49 +0900

    예제 추가

--- 11_interface/main.go
+++ 11_interface/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"golang/11_interface/gadget"
+	"fmt"
+	"golang/11_interface/mypkg"
 )
 
-func playList(device gadget.TapePlayer, songs []string) {
-	for _, song := range songs {
-		device.Play(song)
-	}
-	device.Stop()
-}
-
 func main() {
-	player := gadget.TapePlayer{}
-	mixtape := []string{"Jessie's Girl", "Whip It", "9 to 5"}
-	playList(player, mixtape)
+	var value mypkg.MyInterface
+	value = mypkg.MyType(5)
+	value.MethodWithoutParameters()
+	value.MethodWithParameter(127.3)
+	fmt.Println(value.MethodWithReturnValue())
 }
